f15dac091f36317b56b39dcbcb6a4d1934f29ac1
Ariunzayaaaa <zzzaya98@gmail.com>
2024-01-10 09:41:12 +0900

    update

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 const fs = require('fs');
 const dotenv = require('dotenv');
-const {create} = require('./message');
+const message = require('./message');
 
-dotenv.config();
-
-const name = process.env.NAME;
-const number = process.env.NUMBER;
-
-const message = create(name, number);
-
-fs.writeFile('result.txt', message, (err) => {
-    if(err) {
-        console.error('ファイルの書き込みエラー：', err);
-    }
-    else {
-        console.log('結果をresult.txtに書き込みました。');
-    }
-});
+dotenv. config();
+function create(name, number) {
+    const result = message.create(name, number);
+  
+    fs.writeFileSync('result.txt', result);
+  
+    console.log(result.txt);
+  }
+  
+  const name = process.env.NAME;
+  const number = process.env.NUMBER;
+  
+  create(name, number);
